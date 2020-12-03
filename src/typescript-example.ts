11a64ec72ec487797c069e3ce52f4ca010542e2a
Aniruddha Prasad <anidaniel@erza.com>
2020-12-04 00:03:20 +0530

    changes4error

--- src/typescript-example.ts
+++ src/typescript-example.ts
@@ -17,11 +17,11 @@ function add(a: number, b: number): number {
   return a + b
 }
 
-interface    User {
-  name:  {
-    first: string
+interface   User {
+  name: {
+    first:   string
     middle: string
-    last: string
+    last:    string
   }
 }
 
@@ -29,7 +29,7 @@ function getFullName(user: User): string {
   const {
     name: {first, middle, last},
   } = user
-  return [first,   middle, last].filter(Boolean).join('')
+  return [first, middle, last].filter(Boolean).join('')
 }
 
 add(1, 2)
