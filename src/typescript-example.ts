93e30fff9d6bcc55a7249f895ad2379fde396586
anidaniel <theanidaniel@gmail.com>
2020-12-03 22:49:01 +0530

    anidaniel

--- src/typescript-example.ts
+++ src/typescript-example.ts
@@ -12,6 +12,7 @@
 // add(1, 'two')
 // getFullName({name: {first: 'Chandler', midd1e: 'Muriel', last: 'Bing'}})
 
+//TS based same JS code down below: 
 function add(a: number, b: number): number {
   return a + b
 }
@@ -24,11 +25,11 @@ interface User {
   }
 }
 
-function getFullName(user: User): string {
+function     getFullName(user: User): string {
   const {
     name: {first, middle, last},
   } = user
-  return [first, middle, last].filter(Boolean).join('')
+  return [first,    middle, last].filter(Boolean).join('')
 }
 
 add(1, 2)
